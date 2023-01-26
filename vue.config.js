3a396c103e5e2157dd58773e61accb5472947a70
MichaelPatrick-19910 <110223040+MichaelPatrick-19910@users.noreply.github.com>
2023-01-26 12:14:50 +0700

    modify config

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,20 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/production-sub-path/'
-    : '/'
+    : '/',
+    pages: {
+      index: {
+        // entry for the page
+        entry: 'src/index/main.js',
+        // the source template
+        template: 'public/index.html',
+        // output as dist/index.html
+        filename: 'index.html',
+        // when using title option,
+        // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+        title: 'Index Page',
+        // chunks to include on this page, by default includes
+        // extracted common chunks and vendor chunks.
+      }
+    }
 })
